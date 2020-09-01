27787a6ce4ae2be9a86f92dfc6f34ee1e9071ecb
helton.silveira <helton.silveira@ltmfidelidade.com.br>
2020-09-01 14:39:37 -0300

    adiciona - metodo de calculo

--- CalculadoraViagem/app/src/main/java/br/com/heltonsouza/calculadoraviagem/MainActivity.kt
+++ CalculadoraViagem/app/src/main/java/br/com/heltonsouza/calculadoraviagem/MainActivity.kt
@@ -2,11 +2,29 @@ package br.com.heltonsouza.calculadoraviagem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Html
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvResult.text = Html.fromHtml("<sup><small>R$</small></sup> 0")
+
+        btCalcular.setOnClickListener {
+            calcular()
+        }
+    }
+
+    private fun calcular() {
+        var distance:Int = etDistance.text.toString().toInt()
+        var priceFuel = etPriceFuel.text.toString().toInt()
+        var kms = etKm.text.toString().toInt()
+
+        var result =  distance + priceFuel + kms
+
+        tvResult.text = Html.fromHtml("<sup><small>R$</small></sup> $result");
     }
 }
